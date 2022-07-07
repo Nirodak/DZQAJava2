877363d381e4568bdbb50d0a23a927b777bda04a
Nikolay Rodak <rodak1993@gmail.com>
2022-07-07 12:42:15 +0300

    new code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String []args) {
+
+        int costTicket = 100;
+        int bonus = costTicket / 20;
+
+        System.out.println("Бонусы " + bonus);
+
+    }
 }
